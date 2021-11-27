5d3b96d1ea31948e1d6250e928c81401569bd2a2
azuki7898 <hina.apoo.0124@gmail.com>
2021-11-27 16:25:17 +0000

    [Add]devise機能編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  scope module: :public do
+    devise_for :customers
+  end
+
+    devise_for :admins, controllers: {
+      sessions: 'admin/sessions',
+      passwords: 'admin/passwords'
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
