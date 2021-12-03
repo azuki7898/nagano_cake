b2c78c8fd28ea2949726a0743450ebc394a98efc
azuki7898 <hina.apoo.0124@gmail.com>
2021-12-03 06:23:19 +0000

    [Add]商品新規登録終了

--- db/migrate/20211128155754_create_items.rb
+++ db/migrate/20211128155754_create_items.rb
@@ -1,13 +1,13 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      
+
       t.integer :genre_id
       t.string :name
       t.string :image_id
       t.text :introduction
       t.integer :price
-      t.boolean :is_active
+      t.boolean :is_active, null: false, default:
 
       t.timestamps
     end
