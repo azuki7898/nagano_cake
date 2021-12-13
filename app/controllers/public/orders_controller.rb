9416f0d80a2a156f4c351c05685b0b6080d8f3be
azuki7898 <hina.apoo.0124@gmail.com>
2021-12-13 14:26:16 +0000

    [Add]配送先一覧、編集画面完了、注文情報入力途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,17 +1,19 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = current_customer
+    @customer = current_customer
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   def comfirm
   end
-  
+
   def thanks
   end
-  
+
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
-  
-  belongs_to :admin
+
+  belongs_to :customer
 end
